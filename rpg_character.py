f6f6daa5f1333476ddfb523c28803be9e8e5193a
whozzzdaniel <zzowich@gmail.com>
2026-07-30 22:25:04 +0300

    Выполнил практическое задание "Генератор РПГ персонажа "

---
+++ rpg_character.py
@@ -0,0 +1,31 @@
+full_dot = '●'
+empty_dot = '○'
+
+
+def create_character(name, strength, intelligence, charisma):
+	if not isinstance(name, str):
+		return "The character name should be a string"
+
+	if not name:
+		return "The character should have a name"
+
+	if len(name) > 10:
+		return "The character name is too long"
+
+	if " " in name:
+		return "The character name should not contain spaces"
+
+	if not isinstance(strength, int) or not isinstance(intelligence, int) or not isinstance(charisma, int):
+		return "All stats should be integers"
+
+	if strength < 1 or intelligence < 1 or charisma < 1:
+		return "All stats should be no less than 1"
+
+	if strength > 4 or intelligence > 4 or charisma > 4:
+		return "All stats should be no more than 4"
+
+	if strength + intelligence + charisma != 7:
+		return "The character should start with 7 points"
+
+	return f"{name}\nSTR {strength * full_dot}{(10 - strength) * empty_dot}\nINT \
+{intelligence * full_dot}{(10 - intelligence) * empty_dot}\nCHA {charisma * full_dot}{(10 - charisma) * empty_dot}"
